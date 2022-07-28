765cefe791b49f4bd85d134a6fe3562b4b45fb3f
Anilduz <anilduz34@gmail.com>
2022-07-28 16:30:23 +0300

    push-dev

---
+++ README.txt
@@ -0,0 +1,6 @@
+Please add a readme.txt file which includes group members name, school number and section in the project file.
+
+Anıl Düz G1910.033028 Section 3 
+Eyüp Kaan Özteris G1910.033018 Section 3
+
+Our site address => https://sudobella.xyz/ or https://www.sudobella.xyz/
---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+/*!
+* Start Bootstrap - Scrolling Nav v5.0.4 (https://startbootstrap.com/template/scrolling-nav)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+function addtochart(){
+    
+    alert("ürün sepete eklenmiştir");
+    
+}
+var myForm = document.getElementById('formID');
+myForm.onsubmit = function() {
+   alert("Form isteğiniz destek ekibine ulaştırılmıştır. En kısa sürede sizinle irtibata geçilecektir. Bizi tercih ettiğiniz için teşekkürler.")
+    
+   ;
+};
